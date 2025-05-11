3fd049ff4f71ad335710ae0b63beaaa720b467b5
JimRh <36230932+JimRh@users.noreply.github.com>
2025-05-11 22:06:01 +0600

    deploy6

--- server/server.js
+++ server/server.js
@@ -56,4 +56,4 @@ app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
 
-app.listen(PORT,'0.0.0.0', () => console.log(`Server is now running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
